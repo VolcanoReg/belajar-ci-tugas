9471bd2fea2d38c59e413378450a614be7e90f59
VolcanoReg <nandosite16@gmail.com>
2025-06-04 22:23:39 +0700

    first commit

---
+++ app/Database/Seeds/ProductCategorySeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class ProductCategorySeeder extends Seeder
+{
+    public function run()
+    {
+        // membuat data
+        $data = [
+            [
+                'kategori' => 'Laptop'
+            ],
+            [
+                'kategori' => 'Smartphone'
+            ],
+            [
+                'kategori' => 'PC'
+            ]
+        ];
+
+        foreach ($data as $item) {
+            // insert semua data ke tabel
+            $this->db->table('product_category')->insert($item);
+        }
+    }
+}
